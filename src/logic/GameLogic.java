8c398846b10ff775d6e040bb0eea74f187dc0a40
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-20 19:18:34 +0700

    Add more fields

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -4,9 +4,13 @@ public class GameLogic {
 	
 	private boolean touch;
 	private boolean touchNote;
+	private int combo;
+	private int score;
 	
 	public GameLogic() {
 		touch = false;
 		touchNote = false;
+		combo = 0;
+		score = 0;
 	}
 }
