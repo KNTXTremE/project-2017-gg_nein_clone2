c72a622091b8d52c92e69549fd5ef74b5a823856
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-20 19:42:44 +0700

    Move code to GameModel instead of GameLogic

---
+++ src/logic/GameModel.java
@@ -0,0 +1,62 @@
+package logic;
+
+public class GameModel {
+	
+	private boolean touch;
+	private boolean touchNote;
+	private int combo;
+	private int score;
+	public static final int SCORE_PER_NOTE = 100;
+	public static final int MULTIPILER = 15;
+	
+	public GameModel() {
+		touch = false;
+		touchNote = false;
+		combo = 0;
+		score = 0;
+	}
+
+	public void scoreUp() {
+		if(touchNote == true) {
+			score += SCORE_PER_NOTE + combo*MULTIPILER;
+			combo++;
+		}
+	}
+	
+	public void resetCombo() {
+		combo = 0;
+	}
+	
+	public boolean isTouch() {
+		return touch;
+	}
+
+	public void setTouch(boolean touch) {
+		this.touch = touch;
+	}
+
+	public boolean isTouchNote() {
+		return touchNote;
+	}
+
+	public void setTouchNote(boolean touchNote) {
+		this.touchNote = touchNote;
+	}
+
+	public int getCombo() {
+		return combo;
+	}
+
+	public void setCombo(int combo) {
+		this.combo = combo;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
