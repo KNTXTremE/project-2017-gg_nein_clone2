56f9c0e67c5491c8a78254ba4d24e52aa1dac222
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-20 19:20:20 +0700

    Add getter/setter

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -13,4 +13,38 @@ public class GameLogic {
 		combo = 0;
 		score = 0;
 	}
+
+	public boolean isTouch() {
+		return touch;
+	}
+
+	public void setTouch(boolean touch) {
+		this.touch = touch;
+	}
+
+	public boolean isTouchNote() {
+		return touchNote;
+	}
+
+	public void setTouchNote(boolean touchNote) {
+		this.touchNote = touchNote;
+	}
+
+	public int getCombo() {
+		return combo;
+	}
+
+	public void setCombo(int combo) {
+		this.combo = combo;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
 }
