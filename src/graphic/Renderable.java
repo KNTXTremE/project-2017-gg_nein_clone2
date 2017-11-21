f3eb5dd697043558027520e5040ef314c30af205
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-21 19:01:11 +0700

    Add new font obj

--- src/graphic/Renderable.java
+++ src/graphic/Renderable.java
@@ -6,8 +6,9 @@ import javafx.scene.text.Font;
 public interface Renderable {
 	public static final int SCENE_WIDTH = 800;
 	public static final int SCENE_HEIGHT = 600;
+	public static final Font TEXT_FONT = new Font(50);
 	public static final Font MAINMENU_FONT = new Font("Times new Roman Bold", 50);
-	//public static final Font MAINMENU_FONT = new Font("TH", 50);
+	public static final Font DISPLAYMODE_FONT = new Font(50);
 	public void setBackGround(GraphicsContext gc);
 	public void setText(GraphicsContext gc);
 }
