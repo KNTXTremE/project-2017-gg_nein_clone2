6a78e48c9943cecf5065a3c6db309ebafba01dbd
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-21 21:10:10 +0700

    Add methods to this class

--- src/graphic/DisplaySongSelect.java
+++ src/graphic/DisplaySongSelect.java
@@ -1,7 +1,46 @@
 package graphic;
 
+import application.GameMain;
+import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import window.SceneManager;
 
 public class DisplaySongSelect extends Canvas implements Renderable {
-
+	
+	private GraphicsContext gc = this.getGraphicsContext2D();
+	
+	public DisplaySongSelect() {
+		super(SCENE_WIDTH, SCENE_HEIGHT);
+		setBackGround();
+		setText();
+		EventHandler();
+	}
+	
+	public void setBackGround(){
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+	}
+	
+	public void setText(){
+		gc.setFill(Color.YELLOWGREEN);
+		gc.setFont(TEXT_FONT);
+		gc.fillText("TEST Song Select", SCENE_WIDTH/3, SCENE_HEIGHT/2);
+	}
+	
+	private void EventHandler() {
+		setOnKeyPressed(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent event) {
+                if(event.getCode().equals(KeyCode.ESCAPE)){
+                	GameMain.gotoModeSelect();
+                }
+                else if(event.getCode().equals(KeyCode.ENTER)){
+                	GameMain.gotoInGame();
+                }
+			}
+		}); 
+	}
 }
