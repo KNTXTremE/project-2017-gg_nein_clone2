1e8760ef151753601efa7004ae4f6f7d1adebd4d
Kantanat Siripipatworakun <phantomktn@gmail.com>
2017-11-21 16:04:55 +0700

    edit displaymode

--- src/graphic/DisplayModeSelect.java
+++ src/graphic/DisplayModeSelect.java
@@ -1,7 +1,31 @@
 package graphic;
 
+import java.awt.FontFormatException;
+
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import window.SceneManager;
 
 public class DisplayModeSelect extends Canvas implements Renderable {
-
+	
+	private GraphicsContext gc = this.getGraphicsContext2D();
+	
+	public DisplayModeSelect() {
+		super(SceneManager.SCENE_WIDTH, SceneManager.SCENE_HEIGHT);
+		setBackGround(gc);
+		setText(gc);
+		//EventHandler();
+	}
+	
+	public void setBackGround(GraphicsContext gc){
+		gc.setFill(Color.DARKBLUE);
+		gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+	}
+	public void setText(GraphicsContext gc){
+	gc.setFill(Color.YELLOWGREEN);
+	gc.setFont(DisplayMode_font);
+	}
 }
