7a076a020494480d4fb4d9a2b91ed9f09f078b87
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-21 14:23:47 +0700

    Remove all start method to call from Main and add Constructor in MainMenu

--- src/graphic/MainMenu.java
+++ src/graphic/MainMenu.java
@@ -12,8 +12,18 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import window.SceneManager;
 
 public class MainMenu extends Canvas implements Renderable {
+	
+	private GraphicsContext gc = this.getGraphicsContext2D();
+	
+	public MainMenu() {
+		super(SceneManager.SCENE_WIDTH, SceneManager.SCENE_HEIGHT);
+		setBackGround(gc);
+		setText(gc);
+		EventHandler();
+	}
 	
 	public void setBackGround(GraphicsContext gc) {
 		gc.setFill(Color.BLACK);
@@ -23,36 +33,21 @@ public class MainMenu extends Canvas implements Renderable {
 	public void setText(GraphicsContext gc) {
 		gc.setFont(start_font);
 		gc.setFill(Color.AQUA);
-
 		gc.fillText("Welcome to LapLap Game", SCENE_WIDTH/5, SCENE_HEIGHT/2);
 		gc.setTextAlign(TextAlignment.CENTER);
+		EventHandler();
 	}
-	public void start(Stage firstStage) {
-		Canvas cv = new Canvas(SCENE_WIDTH, SCENE_HEIGHT);
-		StackPane groot = new StackPane();
-		Scene sc = new Scene(groot);
-		GraphicsContext gc = cv.getGraphicsContext2D();
-		
-		setBackGround(gc);
-		setText(gc);
-		groot.getChildren().addAll(cv);
-		firstStage.setScene(sc);
-		firstStage.setTitle("LapLap");
-		firstStage.show();
-		EventHandler(sc,cv);
-		}
-	private void EventHandler(Scene seen,Canvas kanwas) {
-		seen.setOnKeyPressed(new EventHandler<KeyEvent>() {
+	
+	private void EventHandler() {
+		setOnKeyPressed(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent event) {
                 if(event.getCode().equals(KeyCode.ESCAPE)){
-		    	Platform.exit(); }
-			} });
-		kanwas.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			public void handle(KeyEvent event) {
-                if(event.getCode().equals(KeyCode.ENTER)){
-		    	 primarystage.setscene()}
-			} }); 
-		}
-	public static void main(String[] args) {
-		launch(args); }
+                	Platform.exit();
+                }
+                else if(event.getCode().equals(KeyCode.ENTER)){
+//		    	 primarystage.setscene();
+                }
+			}
+		}); 
+	}
 }
