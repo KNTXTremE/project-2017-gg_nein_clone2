f5c182d44e06dc25bbc487ec65342fdaa895d9bf
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-21 15:32:40 +0700

    Set font to Times New Roman

--- src/graphic/Renderable.java
+++ src/graphic/Renderable.java
@@ -6,7 +6,7 @@ import javafx.scene.text.Font;
 public interface Renderable {
 	public static final int SCENE_WIDTH = 800;
 	public static final int SCENE_HEIGHT = 600;
-	public static final Font MAINMENU_FONT = new Font(50);
+	public static final Font MAINMENU_FONT = new Font("Times new Roman", 50);
 	public void setBackGround(GraphicsContext gc);
 	public void setText(GraphicsContext gc);
 }
