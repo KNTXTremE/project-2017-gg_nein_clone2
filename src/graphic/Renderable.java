4b56746ef4959a95d074b5d6bb56ee2b3500b02d
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-22 18:12:28 +0700

    Add Ingame Width and Height and change Scene Width n Height to original

--- src/graphic/Renderable.java
+++ src/graphic/Renderable.java
@@ -7,8 +7,10 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.text.Font;
 
 public interface Renderable {
-	public static final int SCENE_WIDTH = 600;
-	public static final int SCENE_HEIGHT = 1000;
+	public static final int SCENE_WIDTH = 800;
+	public static final int SCENE_HEIGHT = 600;
+	public static final int INGAME_WIDTH = 600;
+	public static final int INGAME_HEIGHT = 1000;
 	public static final Font TEXT_FONT = new Font(50);
 	public static final Font MAINMENU_FONT = new Font("Times new Roman Bold", 50);
 	public static final Font DISPLAYMODE_FONT = new Font(50);
