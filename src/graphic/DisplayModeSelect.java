ea28f362cf8b26ee7d7de2b880a21151f4d5f9a8
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-22 17:50:44 +0700

    Space between methods

--- src/graphic/DisplayModeSelect.java
+++ src/graphic/DisplayModeSelect.java
@@ -21,15 +21,18 @@ public class DisplayModeSelect extends Canvas implements Renderable {
 		setText();
 		EventHandler();
 	}
+	
 	public void setBackGround(){
 		gc.setFill(Color.BLACK);
 		gc.fillRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
 	}
+	
 	public void setText(){
 		gc.setFill(Color.YELLOWGREEN);
 		gc.setFont(TEXT_FONT);
 		gc.fillText("TEST Mode Select", SCENE_WIDTH/2, SCENE_HEIGHT/2);
 	}
+	
 	private void EventHandler() {
 		setOnKeyPressed(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent event) {
