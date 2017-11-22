4058165ae13d76a9c7cff45a247e4c70bc9f9491
KNTXTremE <Kan.Horwang@gmail.com>
2017-11-22 18:11:51 +0700

    Add InGame and GameResult test canvas

--- src/graphic/GameResult.java
+++ src/graphic/GameResult.java
@@ -1,7 +1,46 @@
 package graphic;
 
+import application.GameMain;
+import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import window.SceneManager;
 
 public class GameResult extends Canvas implements Renderable {
-
+	
+	private GraphicsContext gc = this.getGraphicsContext2D();
+	
+	public GameResult() {
+		super(SCENE_WIDTH, SCENE_HEIGHT);
+		setBackGround();
+		setText();
+		EventHandler();
+	}
+	
+	public void setBackGround(){
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+	}
+	
+	public void setText(){
+		gc.setFill(Color.YELLOWGREEN);
+		gc.setFont(TEXT_FONT);
+		gc.fillText("TEST Game Result", SCENE_WIDTH/2, SCENE_HEIGHT/2);
+	}
+	
+	private void EventHandler() {
+		setOnKeyPressed(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent event) {
+                if(event.getCode().equals(KeyCode.ESCAPE)){
+                	SceneManager.gotoMainMenu();
+                }
+                else if(event.getCode().equals(KeyCode.ENTER)){
+                	SceneManager.gotoMainMenu();
+                }
+			}
+		}); 
+	}
 }
