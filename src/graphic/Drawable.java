78b480f703945f4e9defd805514a7070cbfbe174
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-03 01:44:40 +0700

    Change InGame canvas size

--- src/graphic/Drawable.java
+++ src/graphic/Drawable.java
@@ -8,7 +8,7 @@ import javafx.scene.text.Font;
 public interface Drawable {
 	public static final int SCENE_WIDTH = 800;
 	public static final int SCENE_HEIGHT = 600;
-	public static final int INGAME_WIDTH = 400;
+	public static final int INGAME_WIDTH = 480;
 	public static final int INGAME_HEIGHT = 800;
 	public static final Font TEXT_FONT = new Font(50);
 	public static final Font MAINMENU_FONT = new Font("Times new Roman Bold", 50);
