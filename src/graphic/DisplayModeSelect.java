28ef77c438022c2eef51fb6d4c1ee76bd8f5e545
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-04 16:09:09 +0700

    Add options to use keyboard's arrow to select mode

--- src/graphic/DisplayModeSelect.java
+++ src/graphic/DisplayModeSelect.java
@@ -15,40 +15,81 @@ import window.SceneManager;
 public class DisplayModeSelect extends GameMain implements Drawable {
 
 	private GraphicsContext gc = this.getGraphicsContext2D();
-	
+	private String easy_mode = "Easy";
+	private String normal_mode = "Normal";
+	private String hard_mode = "Hard";
+	private int now_mode = 0;
+	double easy_width = fontLoader.computeStringWidth(easy_mode, MAINMENU_FONT);
+	double normal_width = fontLoader.computeStringWidth(normal_mode, MAINMENU_FONT);
+	double hard_width = fontLoader.computeStringWidth(hard_mode, MAINMENU_FONT);
+
 	public DisplayModeSelect() {
 		super(SCENE_WIDTH, SCENE_HEIGHT);
 		setBackGround();
 		setText();
 		EventHandler();
 	}
-	
-	public void setBackGround(){
+
+	public void setBackGround() {
 		gc.setFill(Color.BLACK);
 		gc.drawImage(RenderableHolder.menuBackground, 0, 0);
 	}
-	
-	public void setText(){
-		String title = "Select Your Mode";
-		String diff = "Easy  Normal  Hard  Custom";
+
+	public void setText() {
+		String title = "Select the difficulity";
 		double title_width = fontLoader.computeStringWidth(title, MAINMENU_FONT);
-		double diff_width = fontLoader.computeStringWidth(diff, MAINMENU_FONT);
 		gc.setFont(MAINMENU_FONT);
+		gc.setFill(Color.WHITE);
+		gc.fillText(title, (SCENE_WIDTH - title_width) / 2, SCENE_HEIGHT / 5);
+		gc.fillText(easy_mode, (SCENE_WIDTH - easy_width) / 2, 2 * SCENE_HEIGHT / 5);
+		gc.fillText(normal_mode, (SCENE_WIDTH - normal_width) / 2, 3 * SCENE_HEIGHT / 5);
+		gc.fillText(hard_mode, (SCENE_WIDTH - hard_width) / 2, 4 * SCENE_HEIGHT / 5);
 		gc.setFill(Color.LIME);
-		gc.fillText(title, (SCENE_WIDTH - title_width)/2, SCENE_HEIGHT/3);
-		gc.fillText(diff, (SCENE_WIDTH - diff_width)/2, 2*SCENE_HEIGHT/3);
+		gc.fillText(easy_mode, (SCENE_WIDTH - easy_width) / 2, 2 * SCENE_HEIGHT / 5);
 	}
-	
+
 	private void EventHandler() {
 		setOnKeyPressed(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent event) {
-                if(event.getCode().equals(KeyCode.ESCAPE)){
-                	SceneManager.gotoMainMenu();
-                }
-                else if(event.getCode().equals(KeyCode.ENTER)){
-                	GameMain.gotoSongSelect();
-                }
+				if (event.getCode().equals(KeyCode.ESCAPE)) {
+					SceneManager.gotoMainMenu();
+				} else if (event.getCode().equals(KeyCode.ENTER)) {
+					//model.setMode(now_mode);
+					System.out.println(now_mode);
+					GameMain.gotoSongSelect();
+				} else if (event.getCode().equals(KeyCode.DOWN)) {
+						if (now_mode == 0) {
+							gc.setFill(Color.LIME);
+							gc.fillText(normal_mode, (SCENE_WIDTH - normal_width) / 2, 3 * SCENE_HEIGHT / 5);
+							gc.setFill(Color.WHITE);
+							gc.fillText(easy_mode, (SCENE_WIDTH - easy_width) / 2, 2 * SCENE_HEIGHT / 5);
+							now_mode = 1;
+						} else if (now_mode == 1) {
+							gc.setFill(Color.LIME);
+							gc.fillText(hard_mode, (SCENE_WIDTH - hard_width) / 2, 4 * SCENE_HEIGHT / 5);
+							gc.setFill(Color.WHITE);
+							gc.fillText(normal_mode, (SCENE_WIDTH - normal_width) / 2, 3 * SCENE_HEIGHT / 5);
+							now_mode = 2;
+						}
+
+				} else if (event.getCode().equals(KeyCode.UP)) {
+					
+						if (now_mode == 1) {
+							gc.setFill(Color.WHITE);
+							gc.fillText(normal_mode, (SCENE_WIDTH - normal_width) / 2, 3 * SCENE_HEIGHT / 5);
+							gc.setFill(Color.LIME);
+							gc.fillText(easy_mode, (SCENE_WIDTH - easy_width) / 2, 2 * SCENE_HEIGHT / 5);
+							now_mode = 0;
+						} else if (now_mode == 2) {
+							gc.setFill(Color.WHITE);
+							gc.fillText(hard_mode, (SCENE_WIDTH - hard_width) / 2, 4 * SCENE_HEIGHT / 5);
+							gc.setFill(Color.LIME);
+							gc.fillText(normal_mode, (SCENE_WIDTH - normal_width) / 2, 3 * SCENE_HEIGHT / 5);
+							now_mode = 1;
+						}
+
+				}
 			}
-		}); 
+		});
 	}
 }
