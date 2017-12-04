889824717b7e9b06329cfe7e3bad6e19079fca61
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-04 19:59:06 +0700

    Remove unuse font and add new Font family

--- src/graphic/Drawable.java
+++ src/graphic/Drawable.java
@@ -10,10 +10,9 @@ public interface Drawable {
 	public static final int SCENE_HEIGHT = 600;
 	public static final int INGAME_WIDTH = 480;
 	public static final int INGAME_HEIGHT = 800;
-	public static final Font TEXT_FONT = new Font(50);
-	public static final Font MAINMENU_FONT = new Font("Times new Roman Bold", 50);
-	public static final Font DISPLAYMODE_FONT = new Font(50);
-	FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
+	public static final Font MAIN_FONT = new Font("Monospace", 50);
+	public static final Font SUBMAIN_FONT = new Font("Monospace", 25);
+	public static FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
 	public void setBackGround();
 	public void setText();
 }
