88ae7f658307482e964e9e966233392e442f1620
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-05 20:16:49 +0700

    Add default value to time for test

--- src/logic/CountDownTimer.java
+++ src/logic/CountDownTimer.java
@@ -4,9 +4,11 @@ public class CountDownTimer {
 	
 	private long time;
 	private static final long DEFAULT_TIME = 65000000000L;
+	private static final int PER_MINUTE = 60;
 	
-	public CountDownTimer(int time) {
-		this.time = time;
+	public CountDownTimer(long time) {
+		//IF ALREADY ADD SONG, PLS CHANGE THIS LINE TO READ SONG LENGTH INSTEAD
+		this.time = DEFAULT_TIME;
 	}
 	
 	public void countDown(long elapsedTime) {
