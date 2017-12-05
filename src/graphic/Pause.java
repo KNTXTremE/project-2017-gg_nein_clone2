ac2a069ec2c5f3ceca47e09e7d376a325c184740
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-05 20:51:39 +0700

    Implements neccessary methods

--- src/graphic/Pause.java
+++ src/graphic/Pause.java
@@ -2,4 +2,34 @@ package graphic;
 
 public class Pause extends CanvasManager implements Drawable {
 
+
+	@Override
+	public void setBackGround() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setText() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setHightLight(String selected_mode, double selected_width) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUnHightLight(String unsel_mode, double unsel_width) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void EventHandler() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
