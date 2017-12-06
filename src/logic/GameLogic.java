72f0d378f27741dcf371fedfc62bb42b4a6c350a
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-06 20:02:43 +0700

    Add default value for field

--- src/logic/GameLogic.java
+++ src/logic/GameLogic.java
@@ -14,6 +14,7 @@ public class GameLogic {
 	public GameLogic(InGame ingame) {
 		model = CanvasManager.getModel();
 		this.ingame = ingame;
+		isGameRunning = false;
 	}
 
 	public void startGame() {
