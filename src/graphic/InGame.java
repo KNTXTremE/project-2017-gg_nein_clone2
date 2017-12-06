5543be8f3f4e50103b915590a7cf3e1d3971d985
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-06 13:14:33 +0700

    Remove unuse import

--- src/graphic/InGame.java
+++ src/graphic/InGame.java
@@ -5,7 +5,6 @@ import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import logic.GameModel;
 import sharedObject.RenderableHolder;
 import window.SceneManager;
 
