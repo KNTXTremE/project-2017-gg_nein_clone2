0cc5e7028bd23f9b45b1c836abeafd90d7b6b0cd
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-06 10:46:12 +0700

    Implements the highlight methods

--- src/graphic/DisplaySongSelect.java
+++ src/graphic/DisplaySongSelect.java
@@ -45,13 +45,15 @@ public class DisplaySongSelect extends CanvasManager implements Drawable {
 	@Override
 	public void setHightLight(String selected_mode, double selected_width) {
 		// TODO Auto-generated method stub
-		
+		gc.setFill(Color.LIME);
+		gc.fillText(selected_mode, (SCENE_WIDTH - selected_width) / 2, (2 + model.getMode()) * SCENE_HEIGHT / (5 + model.getAllSongs().size()));
 	}
 
 	@Override
 	public void setUnHightLight(String unsel_mode, double unsel_width) {
 		// TODO Auto-generated method stub
-		
+		gc.setFill(Color.ANTIQUEWHITE);
+		gc.fillText(unsel_mode, (SCENE_WIDTH - unsel_width) / 2, (2 + model.getMode()) * SCENE_HEIGHT / (5 + model.getAllSongs().size()));
 	}
 	
 	private void EventHandler() {
