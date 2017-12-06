a77167398e230235de1e76aa0d63a30e40d17b5a
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-06 17:57:49 +0700

    Add draw in Renderable interface

--- src/sharedObject/Renderable.java
+++ src/sharedObject/Renderable.java
@@ -1,6 +1,9 @@
 package sharedObject;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public interface Renderable {
 	public int getZ();
+	public void draw(GraphicsContext gc);
 	public boolean isPressed();
 }
