d2c0ba1753ca9f8b0b5358313812c7dd479ba38c
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-07 20:12:28 +0700

    Change position that relate to the time (Still not OK)

--- src/logic/Note.java
+++ src/logic/Note.java
@@ -1,16 +1,19 @@
 package logic;
 
+import graphic.CanvasManager;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class Note extends Items {
 
 	private int position;
+	private GameModel model;
 	
 	public Note(double y, int position) {
 		this.position = position;
 		this.y = y;
 		this.z = 100;
+		model = CanvasManager.getModel();
 	}
 	
 	@Override
@@ -18,8 +21,8 @@ public class Note extends Items {
 		// TODO Auto-generated method stub
 		gc.setFill(Color.YELLOW);
 		gc.setStroke(Color.YELLOWGREEN);
-		gc.fillOval(90 + (position - 1)*120, y*100, 50, 50);
-		gc.strokeOval(90 + (position - 1)*120, y*100, 50, 50);
+		gc.fillOval(90 + (position - 1)*120, (y*model.getSpeed() + model.getAllSongs().get(model.getSelectedSong()).getSongDuration() - model.getCountDownTimer().getTimeSecond()), 50, 50);
+		gc.strokeOval(90 + (position - 1)*120, (y*model.getSpeed() + model.getAllSongs().get(model.getSelectedSong()).getSongDuration() - model.getCountDownTimer().getTimeSecond()), 50, 50);
 	}
 
 }
