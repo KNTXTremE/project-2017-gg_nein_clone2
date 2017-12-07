b62dfa9d3f992fdcee592ac752053b424a0d913e
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-07 16:39:01 +0700

    Implements note class and draw it

--- src/logic/Note.java
+++ src/logic/Note.java
@@ -1,13 +1,24 @@
 package logic;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class Note extends Items {
 
+	private int position;
+	
+	public Note(double y, int position) {
+		this.position = position;
+		this.y = y;
+	}
+	
 	@Override
 	public void draw(GraphicsContext gc) {
 		// TODO Auto-generated method stub
-		
+		gc.setFill(Color.YELLOW);
+		gc.setStroke(Color.YELLOWGREEN);
+		gc.fillOval(position*100, y, 50, 50);
+		gc.strokeOval(position*100, y, 50, 50);
 	}
 
 }
