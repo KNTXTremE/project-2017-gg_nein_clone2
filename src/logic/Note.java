2c7a4bfa36925cf0b7edb843c6d03506d13fab58
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-08 14:45:16 +0700

    Rearrange line

--- src/logic/Note.java
+++ src/logic/Note.java
@@ -6,24 +6,31 @@ import javafx.scene.paint.Color;
 public class Note extends Items {
 
 	private int position;
-	
-	
+
 	public Note(double y, int position) {
 		this.position = position;
 		this.y = y;
 		this.z = 100;
 	}
-	
+
 	@Override
 	public void draw(GraphicsContext gc, int count) {
 		// TODO Auto-generated method stub
 		gc.setFill(Color.YELLOW);
 		gc.setStroke(Color.YELLOWGREEN);
-		double Y = count*model.getSpeed() + 120*(model.getAllSongs().get(model.getSelectedSong()).getSongDuration() - y)  - 3500; //MINUS FOR GOING UP
-		gc.fillOval(90 + (position - 1)*120, Y, 50, 50);
-		gc.strokeOval(90 + (position - 1)*120, Y, 50, 50);
-//		gc.fillOval(90 + (position - 1)*120, (y*model.getSpeed() + model.getAllSongs().get(model.getSelectedSong()).getSongDuration() - model.getCountDownTimer().getTimeSecond()), 50, 50);
-//		gc.strokeOval(90 + (position - 1)*120, (y*model.getSpeed() + model.getAllSongs().get(model.getSelectedSong()).getSongDuration() - model.getCountDownTimer().getTimeSecond()), 50, 50);
+		double Y = count * model.getSpeed()
+				+ 120 * (model.getAllSongs().get(model.getSelectedSong()).getSongDuration() - y) - 3500; // MINUS FOR
+																											// GOING UP
+		gc.fillOval(90 + (position - 1) * 120, Y, 50, 50);
+		gc.strokeOval(90 + (position - 1) * 120, Y, 50, 50);
+		// gc.fillOval(90 + (position - 1)*120, (y*model.getSpeed() +
+		// model.getAllSongs().get(model.getSelectedSong()).getSongDuration() -
+		// model.getCountDownTimer().getTimeSecond()), 50, 50);
+		// gc.strokeOval(90 + (position - 1)*120, (y*model.getSpeed() +
+		// model.getAllSongs().get(model.getSelectedSong()).getSongDuration() -
+		// model.getCountDownTimer().getTimeSecond()), 50, 50);
+	}
+
 	}
 
 }
