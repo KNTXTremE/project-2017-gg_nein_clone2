76de5f7dbe210e9ef544c67b26998a43f49b2528
KNTXTremE <Kan.Horwang@gmail.com>
2017-12-08 19:24:41 +0700

    add update method in interface

--- src/sharedObject/Renderable.java
+++ src/sharedObject/Renderable.java
@@ -5,6 +5,7 @@ import javafx.scene.canvas.GraphicsContext;
 public interface Renderable {
 	public int getZ();
 	public void draw(GraphicsContext gc, int count);
+	public void update();
 	public boolean isPressed();
 	public boolean isVisible();
 }
